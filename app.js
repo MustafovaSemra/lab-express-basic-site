079d9af7d629c7be5d05b2b0d16cf44fe68a5003
MustafovaSemra <mustafovasemra@yahoo.com>
2021-07-12 14:39:50 -0400

    addes structure and some css

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+
+app.get("/", (request, response, next) =>
+  response.sendFile(__dirname + "/views/home.html")
+);
+app.get("/about", (request, response, next) =>
+  response.sendFile(__dirname + "/views/about.html")
+);
+app.get("/works", (request, response, next) =>
+  response.sendFile(__dirname + "/views/works.html")
+);
+
+app.get("/photogallery", (request, response, next) =>
+  response.sendFile(__dirname + "/views/photogallery.html")
+);
+
+app.listen(5001);
